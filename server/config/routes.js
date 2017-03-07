dc644b96f8654e0dbc0b86c5f8e1788c771d67bd
Won Jin Chung <chungw51993@gmail.com>
2017-03-07 16:29:08 -0600

    added express.static to route

--- server/config/routes.js
+++ server/config/routes.js
@@ -6,4 +6,5 @@ module.exports = function(app, express) {
   app.use(bodyParser.json());
   app.post('/lists', listController.search);
   app.post('/item', itemController.detail);
+  app.get('/', app.use(express.static(__dirname + '/../client')));
 };
