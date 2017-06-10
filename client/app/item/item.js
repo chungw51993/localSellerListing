8b44c82100003f5e57001a9504f8ecc2005f9bfc
Won Jin Chung <chungw51993@gmail.com>
2017-06-10 13:28:06 -0500

    Fixed styling in item

--- client/app/item/item.js
+++ client/app/item/item.js
@@ -6,13 +6,18 @@ angular.module('localList.item', [])
   $scope.render = function() {
     $scope.item = localStorageService.get('data');
     $scope.posted = $scope.item.data.postedAt.split('T').join(' ').split('.')[0];
-    var location = $scope.item.data.mapUrl.split('@')[1].split(',').splice(0, 2).join(', ');
-    $scope.location = $sce.trustAsResourceUrl('//www.google.com/maps/embed/v1/place?q=' + location + '&zoom=17&key=AIzaSyCiyY-io3zhG_HUPAjVOi1OED2tt3g-POQ');
+    if ($scope.item.data.mapUrl) {
+      if ($scope.item.data.mapUrl.includes('=')) {
+        var location = $scope.item.data.mapUrl.split('=')[1];
+      } else {
+        var location = $scope.item.data.mapUrl.split('@')[1].split(',').splice(0, 2).join(', ');
+      }
+      $scope.location = $sce.trustAsResourceUrl('//www.google.com/maps/embed/v1/place?q=' + location + '&zoom=17&key=AIzaSyCiyY-io3zhG_HUPAjVOi1OED2tt3g-POQ');
+    }
   };
 
   $scope.goToList = function() {
     $location.path('/lists');
   };
-
   $scope.render();
 });
